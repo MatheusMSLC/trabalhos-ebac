d495d6a19995cbdaec7c7bdd36b922a9fbda03bb
MatheusMSLC <155547500+MatheusMSLC@users.noreply.github.com>
2024-01-03 20:35:27 -0300

    Media de notas

---
+++ Tarefa_CalculoMedia/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String args[]) {
+
+        float nota1 = 7.5F;
+        float nota2 = 3.7F;
+        float nota3 = 6.1F;
+        float nota4 = 5.8F;
+
+        float somaNota = nota1 + nota2 + nota3 + nota4;
+
+        float notaFinal = somaNota/4;
+
+        System.out.println("Nota final: " + notaFinal);
+        
+    }
+}
